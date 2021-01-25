f0e59730598a457b6870914614c8f9326aa511d0
RobertoEcheva <52468636+RobertoEcheva@users.noreply.github.com>
2021-01-25 14:41:20 -0400

    node_modules client

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require ("express");
 const app = express();
 const cors = require("cors");
-const pool = require("./db");
+//const pool = require("./db");
+
+//Import de los endpoints
+const pruebas = require ("../routes/pruebas.js")
 
 //middleware
 app.use(cors());
@@ -9,80 +12,82 @@ app.use (express.json()); //req.body
 
 //ROUTES//
 
-//Create Prueba
+// //Create Prueba
 
-app.post("/pruebas", async(req, res)=> {
-    try {
-       const { description } = req.body;
-       const newPrueba = await pool.query(
-           "INSERT INTO prueba (description) VALUES($1) RETURNING *",
-           [description]
-        );
+// app.post("/pruebas", async(req, res)=> {
+//     try {
+//        const { description } = req.body;
+//        const newPrueba = await pool.query(
+//            "INSERT INTO prueba (description) VALUES($1) RETURNING *",
+//            [description]
+//         );
 
-    res.json(newPrueba.rows[0]);
-    } catch (err) {
-        console.error(err.message);
-    }
-});
+//     res.json(newPrueba.rows[0]);
+//     } catch (err) {
+//         console.error(err.message);
+//     }
+// });
 
-//Get all Pruebas
+// //Get all Pruebas
 
-app.get ("/pruebas", async (req, res) =>{
-    try {
-        const allPruebas = await pool.query(
-            "SELECT * FROM prueba"
-        );
-    res.json(allPruebas.rows);
-    } catch (err) {
-        console.error(err.message);
-    }
-})
+// app.get ("/pruebas", async (req, res) =>{
+//     try {
+//         const allPruebas = await pool.query(
+//             "SELECT * FROM prueba"
+//         );
+//     res.json(allPruebas.rows);
+//     } catch (err) {
+//         console.error(err.message);
+//     }
+// })
 
-//Get a Prueba
+// //Get a Prueba
 
-app.get("/pruebas/:id", async(req, res) =>{
-    try {
-        const { id } = req.params;
-        const prueba = await pool.query(
-            "SELECT * FROM prueba WHERE prueba_id = $1",
-            [id]
-        );
-    res.json(prueba.rows[0]);
-    } catch (err) {
-        console.error(err.message);
-    }
-})
+// app.get("/pruebas/:id", async(req, res) =>{
+//     try {
+//         const { id } = req.params;
+//         const prueba = await pool.query(
+//             "SELECT * FROM prueba WHERE prueba_id = $1",
+//             [id]
+//         );
+//     res.json(prueba.rows[0]);
+//     } catch (err) {
+//         console.error(err.message);
+//     }
+// })
 
-//Update a Prueba
+// //Update a Prueba
 
-app.put("/pruebas/:id", async(req,res) =>{
-    try {
-        const { id } = req.params;
-        const { description } = req.body;
-        const updatePrueba = await pool.query(
-            "UPDATE prueba SET description = $1 WHERE  prueba_id = $2",
-            [description, id]
-        );
-    res.json ("Prueba was updated");
-    } catch (err) {
-        console.error(err.message);
-    }
-})
+// app.put("/pruebas/:id", async(req,res) =>{
+//     try {
+//         const { id } = req.params;
+//         const { description } = req.body;
+//         const updatePrueba = await pool.query(
+//             "UPDATE prueba SET description = $1 WHERE  prueba_id = $2",
+//             [description, id]
+//         );
+//     res.json ("Prueba was updated");
+//     } catch (err) {
+//         console.error(err.message);
+//     }
+// })
 
-//Delete a Prueba
+// //Delete a Prueba
 
-app.delete ("/pruebas/:id", async (req,res) =>{
-    try {
-        const { id } = req.params;
-        const deletePrueba = await pool.query(
-            "DELETE FROM prueba WHERE prueba_id = $1",
-            [id]
-        );
-    res.json("Prueba was deleted");
-    } catch (err) {
-        console.error(err.message)
-    }
-})
+// app.delete ("/pruebas/:id", async (req,res) =>{
+//     try {
+//         const { id } = req.params;
+//         const deletePrueba = await pool.query(
+//             "DELETE FROM prueba WHERE prueba_id = $1",
+//             [id]
+//         );
+//     res.json("Prueba was deleted");
+//     } catch (err) {
+//         console.error(err.message)
+//     }
+// })
+
+app.use(pruebas)
 
 app.listen(5000, () =>{
     console.log ("Server has started on port 5000")
