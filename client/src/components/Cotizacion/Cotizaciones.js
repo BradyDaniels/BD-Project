8c7329bc6b5254f6863359b4e58ea6c03f8194be
RobertoEcheva <52468636+RobertoEcheva@users.noreply.github.com>
2021-02-03 16:36:51 -0400

    Front Cotizacion

--- client/src/components/Cotizacion/Cotizaciones.js
+++ client/src/components/Cotizacion/Cotizaciones.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import CotizacionesTable from './CotizacionesTable'
-//import CotizacionesForm from './CotizacionesForm'
+import CotizacionesForm from './CotizacionesForm'
+import CDetalleReqTable from './CDetalleReqTable'
 
 const Cotizaciones = () => {
 
     return (
         <div className="content-container">
             <h1 className="content-title">Cotizaciones</h1>
-            {/* <div className="form-container">
+            <div className="form-container">
                 <CotizacionesForm />
-            </div> */}
-            <div className="table-container">
-                <CotizacionesTable />
             </div>
+            {/* <div className="table-container">
+                <CDetalleReqTable />
+            </div>  */}
         </div>
     )
 }
---
+++ client/src/components/Cotizacion/CotizacionesForm.js
@@ -0,0 +1,242 @@
+import React, { useState, useEffect } from 'react'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import FormControl from '@material-ui/core/FormControl'
+import RadioGroup from '@material-ui/core/RadioGroup'
+import Radio from '@material-ui/core/Radio'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import InputLabel from '@material-ui/core/InputLabel'
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem'
+import useForm from '../useForm/useForm'
+import MaterialTable from 'material-table'
+import CDetalleReqTable from './CDetalleReqTable'
+
+// cedula, id_dependencia, nombre, tipo 
+const CotizacionesForm = () => {
+
+    const [req, setReq] = useState([{}])
+    const [linea, setlinea] = useState([{}])
+    const [proveedor, setProveedores] = useState([{}])
+
+    
+    const [DetalleRequisicion, setDetalleRequisicion] = useState([{}]);
+
+    const [toggle, setToggle] = useState(false)
+
+    const [ state, setState ] = useState({
+        columns: [
+            {title: 'ID', field: 'id_requisicion', editable: 'never'},
+            {title: 'Cantidad Solicitada', field: 'cantidad_solicitada'},
+            {title: 'Precio Estimado', field: 'precio_estimado'},
+            {title: 'ID Item', field: 'id_item'},
+        ],
+        data: []
+    })
+    
+    const proxy = 'cotizacion'
+    const { handleChange, handleSubmit, values } = useForm({
+        id: '',
+        id_linea: '',
+        fecha_emision: getLocalDate(),
+        observaciones: '',
+        condiciones_entrega:'',
+        id_requisicion: '',
+        rif_proveedor: ''
+        //id_cotizacion: ''
+    }, proxy)
+
+
+
+    const toggleSelect = ({ target }) => setToggle(target.value == "" ? true : false)
+
+    const fetchLineas = () => {
+        fetch('http://localhost:5000/lineas_suministro')
+            .then(res => res.json())
+            .then(result => setlinea(result))
+            .catch(err => console.log(err.message))
+    }
+
+    const fetchRequisiciones = () => {
+        fetch('http://localhost:5000/requisiciones')
+            .then(res => res.json())
+            .then(result => setReq(result))
+            .catch(err => console.log(err.message))
+    }
+
+    const fetchProveedores = () => {
+        fetch('http://localhost:5000/proveedores')
+            .then(res => res.json())
+            .then(result => setProveedores(result))
+            .catch(err => console.log(err.message))
+    }
+
+    const fetchDetalleRequisicion = () => {
+        if(values.id_requisicion==''){
+        fetch('http://localhost:5000/detalle_requisicion/')
+            .then(res => res.json())
+            .then(result => setDetalleRequisicion(result))
+            .catch(err => console.log(err.message))
+        }else{
+            const GetItems=fetch(`http://localhost:5000/detalle_requisicion/${values.id_requisicion}`, {
+             method: 'GET',
+             headers: { 'Content-type': 'application/json' }
+         })
+             .then(res => res.json())
+             .then(result => setDetalleRequisicion(result))
+             .catch(err => console.log(err.message))
+        }
+
+    }
+
+    function getLocalDate() {
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+        today = mm + '-' + dd + '-' + yyyy;
+        return today;
+    }
+
+    useEffect(() => {
+        fetchLineas()
+        fetchRequisiciones()
+        fetchDetalleRequisicion()
+        fetchProveedores()
+    }, [])
+
+    return (
+        <div className="form-container">
+            <form onSubmit={handleSubmit}>
+                <FormControl className="form-trabajador">
+                    <div className="form-trabajador-1">
+                        <TextField
+                            className="text-field"
+                            size="small"
+                            label="ID"
+                            name="id"
+                            variant="outlined"
+                            value={values.id}
+                            onChange={handleChange} />
+                        <TextField
+                            className="text-field"
+                            size="small"
+                            label="Fecha de Emision"
+                            name="fecha_emision"
+                            variant="outlined"
+                            value={getLocalDate()} />
+                    </div>
+                    <TextField
+                        className="text-field"
+                        size="small"
+                        label="Observaciones"
+                        name="observaciones"
+                        variant="outlined"
+                        value={values.observaciones}
+                        onChange={handleChange} />
+                        <TextField
+                            className="text-field"
+                            size="small"
+                            label="Condiciones de Entrega"
+                            name="condiciones_entrega"
+                            variant="outlined"
+                            value={values.condiciones_entrega}
+                            onChange={handleChange} />
+                   <FormControl>
+                        <InputLabel id="lineas-label">Linea</InputLabel>
+                        <Select
+                            labelId="lineas-label"
+                            id="linea"
+                            value={values.id_linea}
+                            name="id_linea"
+                            onChange={handleChange}
+                            onBlur={handleChange}
+                        >
+                            {linea.map((lineas, i) => (
+                                <MenuItem value={lineas.id} key={i}>
+                                    {lineas.descripcion}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                    <FormControl>
+                        <InputLabel id="lineas-label">Proveedor</InputLabel>
+                        <Select
+                            labelId="lineas-label"
+                            id="proveedor"
+                            value={values.rif_proveedor}
+                            name="rif_proveedor"
+                            onChange={handleChange}
+                            onBlur={handleChange}
+                        >
+                            {proveedor.map((proveedor, i) => (
+                                <MenuItem value={proveedor.rif} key={i}>
+                                    {proveedor.razon_social}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                    <FormControl>
+                        <InputLabel id="requisicion-label">Requisicion</InputLabel>
+                        <Select
+                            labelId="requisicion-label"
+                            id="requisicion"
+                            value={values.id_requisicion}
+                            name="id_requisicion"
+                            onChange={handleChange}
+                            onBlur={handleChange}
+                        >
+                            {req.map((req, i) => (
+                                <MenuItem value={req.id} key={i}>
+                                    {req.id}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                    <Button type="submit" variant="contained" size="small" disableElevation>Añadir Cotizacion</Button>
+                </FormControl>
+            </form>
+            
+        <MaterialTable
+            title="Detalles de Requisicion"
+            columns={state.columns}
+            data={DetalleRequisicion}
+            options={{
+                filtering: true
+              }}
+            editable={{
+                onRowUpdate: (newData, oldData) =>
+                    new Promise((resolve) => {
+                        setTimeout(() => {
+                            resolve();
+                            if (oldData) {
+                                setState((prevState) => {
+                                    const data = [...prevState.data];
+                                    data[data.indexOf(oldData)] = newData;
+                                    //updateDetalleRequisicion(newData);//AQUI SE ACTUALIZA EL CAMPO
+                                    console.log(newData);
+                                    return { ...prevState, data };
+                                });
+                            }
+                        }, 600);
+                    }),
+                onRowDelete: (oldData) =>
+                    new Promise((resolve) => {
+                        //deleteDetalleRequisicion(oldData.id);//AQUI SE DELETEA LA ESPECIALIDAD
+                        console.log(oldData.id);
+                        setTimeout(() => {
+                            resolve();
+                            setState((prevState) => {
+                                const data = [...prevState.data];
+                                data.splice(data.indexOf(oldData), 1);
+                                return { ...prevState, data };
+                            });
+                        }, 600);
+                    }),
+            }}
+        />
+        </div>
+    )
+}
+
+export default CotizacionesForm
